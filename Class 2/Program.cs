8d852a269e94a071965a8a6043f4688fdd8017a6
dharryc <97463151+dharryc@users.noreply.github.com>
2022-01-14 18:58:11 -0700

    Add files via upload

---
+++ Class 2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+    class Car
+    {
+        public int MaxSpeed;
+        public int Doors;
+        public int Windows;
+        public string CarName;
+    };
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Car SarasCar = new Car();
+            SarasCar.CarName = "benny";
+            Console.WriteLine(SarasCar.CarName);
+            Console.WriteLine(SarasCar);
+        }
+    }
